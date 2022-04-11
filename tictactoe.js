ffc8bc75029232e01f8f8e8080013cc7733cd413
Zach <zachdangelo100@gmail.com>
2022-04-11 11:15:56 -0400

    Display controller and game flow is working properly. Victory conditions are not yet implemented, they will be included in the next commit.

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,42 @@
+
+
 const gameBoard = (() =>{
-    const board = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-    ];
-    const isValidMove = (x,y) => board[x][y] === ''; 
-    
-    return {board, isValidMove};
+    const board = document.querySelectorAll(".space");
+    function checkForWin(){
+        if (checkPlayerWin()){
+            console.log("winner");
+            return true;
+        } 
+        //if (checkComputerWin()) return;
+        //if (checkTie()) return;
+    }
+
+    function checkPlayerWin(){
+        if(checkHorizontalWin('X')) return true;
+        if(checkVerticalWin('Y')) return true;
+    }
+
+    function checkHorizontalWin(symbol){
+        let x = 0;
+        let xCounter = 0;
+        board.forEach(elem=>{
+            if(elem.textContent == symbol){
+                xCounter++;
+            }
+            x++;
+            if(xCounter == 3) return true;
+            if(x >= 3){
+                x = 0;
+                xCounter = 0;
+            }
+        });
+    }
+    function checkVerticalWin(symbol){
+        let x = 0;
+        let y = 0;
+    }
+
+    return {board, checkForWin};
 })();
 
 
@@ -16,15 +46,58 @@ const Player = (playerSymbol) =>{
 };
 
 const displayController = ((document) =>{
+    const boardSpaces = document.querySelectorAll(".space");
+
+    function registerClickEvents(){
+        gameBoard.board.forEach((elem) =>{
+            elem.addEventListener("click", () =>{
+                console.log(elem.textContent);
+                gameState.playerMove(elem)
+            })
+        });
+    }
+
     const updateDisplay = () => {
-        console.log(gameBoard.board);
+        let x = 0;
+        let y = 0;
+        gameBoard.board.forEach((elem)=>{
+           elem.textContent = gameBoard.board[y][x];
+           x++;
+           if(x >= 3){
+               x = 0;
+               y++;
+           }
+        });
+    }
+
+    return {updateDisplay, registerClickEvents};
+})(document);
+
+const gameState = (() =>{
+    const playerOne = Player('X');
+    const playerTwo = Player('O');
+    var currentPlayer = "";
+    (function startGame(){
+        displayController.updateDisplay();
+        currentPlayer = playerOne;
+    })();
+
+    function playerMove(spaceElem){
+        if(spaceElem.textContent) return;
+        else{
+            spaceElem.textContent = currentPlayer.symbol;
+            currentPlayer = (currentPlayer == playerOne) ? playerTwo : playerOne; 
+        }
+        if(gameBoard.checkForWin()){
+            console.log("Winner!");
+        }
+    }
+
+    function computerMove(){
 
     }
 
-    return {updateDisplay};
-})(document);
+    return{playerMove}
+})();
 
-
-const playerOne = Player('X');
-const playerTwo = Player('O');
-displayController.updateDisplay();
+displayController.registerClickEvents();
