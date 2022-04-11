7611d41a4faa53c67e07326a0c806dfaca0714dd
Zach <zachdangelo100@gmail.com>
2022-04-11 10:29:03 -0400

    Added displayController to tictactoe.js and created basic table in html and css files.

--- tictactoe.js
+++ tictactoe.js
@@ -6,5 +6,25 @@ const gameBoard = (() =>{
     ];
     const isValidMove = (x,y) => board[x][y] === ''; 
     
-    return {board, isValidMove}
+    return {board, isValidMove};
 })();
+
+
+const Player = (playerSymbol) =>{
+    const symbol = playerSymbol;
+    return {symbol};
+};
+
+const displayController = ((document) =>{
+    const updateDisplay = () => {
+        console.log(gameBoard.board);
+
+    }
+
+    return {updateDisplay};
+})(document);
+
+
+const playerOne = Player('X');
+const playerTwo = Player('O');
+displayController.updateDisplay();
